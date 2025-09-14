66dcb6d3e90612180556e76870b21e7443f365d5
Fatimeh1 <fatimehahmadi60@gmail.com>
2025-09-14 21:55:20 +0300

    Add new code lines in app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import sqlite3
 from flask import Flask
-from flask import redirect, render_template, request
-from werkzeug.security import generate_password_hash
+from flask import redirect, render_template, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
 import config
 import db 
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
