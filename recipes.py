2b11f5445f2300b72ef1369e1a3a875445a77976
Fatimeh1 <fatimehahmadi60@gmail.com>
2025-11-02 23:46:07 +0200

    fix

--- recipes.py
+++ recipes.py
@@ -20,8 +20,8 @@ def add_recipe(title, ingredients, user_id, classes):
     recipe_id = db.last_insert_id() 
 
     sql = "INSERT INTO recipe_classes (recipe_id, title, value) VALUES (?, ?, ?)"
-    for title, value in classes:
-        db.execute(sql, [recipe_id, title, value]) 
+    for class_title, class_value in classes:
+        db.execute(sql, [recipe_id, class_title, class_value]) 
 
 def add_comment(recipe_id, user_id, comment):
     sql = """INSERT INTO comments (recipe_id, user_id, comment) 
@@ -90,8 +90,8 @@ def update_recipe(recipe_id, title, ingredients, classes):
     db.execute(sql , [recipe_id])
 
     sql = "INSERT INTO recipe_classes (recipe_id, title, value) VALUES (?, ?, ?)"
-    for title, value in classes:
-        db.execute(sql, [recipe_id, title, value])
+    for class_title, class_value in classes:
+        db.execute(sql, [recipe_id, class_title, class_value])
 
 def remove_recipe(recipe_id):
     sql = "DELETE FROM comments WHERE recipe_id = ?"
